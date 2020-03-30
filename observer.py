3d36435cae0fbd5fad8948db5f135a76d2f5e687
doman <domanski@basement.pl>
2020-03-30 16:59:51 +0200

    Proper argparse and logging, multiple options

--- observer.py
+++ observer.py
@@ -1,17 +1,32 @@
+import argparse
 import os
 import sys
 import time
+import pathlib
 import re
 import requests
+import logging
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+log = logging.getLogger()
 
 
 class NewFileHander(FileSystemEventHandler):
     def __init__(
         self, extension_mapper, destination_folder, excluded, *args, **kwargs,
     ):
+        """
+        Handler for observing file modifications.
+
+        extension_mapper should be an object providing .get function, such as dict,
+        default dict or special function, and it should return subfolder name for given
+        destination folder. Also, destination folder can be specified as root folder 
+        and extension mapper can provide absolute paths.
+
+        exluded param should consist of regexes for excluded files
+        """
         super().__init__(*args, **kwargs)
         self.extension_mapper = extension_mapper
         self.destination_folder = destination_folder
@@ -21,32 +36,34 @@ class NewFileHander(FileSystemEventHandler):
         if not os.path.isfile(event.src_path):
             return
 
-        origin = event.src_path.split("/")[-1]
+        origin = event.src_path.split(os.path.sep)[-1]
 
         if any((re.match(pattern, origin) for pattern in self.excluded)):
             return
 
-        print(f"processing {origin}")
+        log.debug(f"processing {origin}")
 
         destination_folder = os.path.join(
             self.destination_folder, self.extension_mapper.get(origin)
         )
 
+        log.debug(f"Destination folder: {destination_folder}")
+
+        pathlib.Path(destination_folder).mkdir(parents=True, exist_ok=True)
+
         try:
             os.rename(
-                event.src_path,
-                os.path.join(destination_folder, origin),
+                event.src_path, os.path.join(destination_folder, origin),
             )
-        except FileNotFoundError:
-            os.mkdir(destination_folder)
-            os.rename(
-                event.src_path,
-                os.path.join(destination_folder, origin),
-            )
+        except Exception as e:
+            log.error(f"processing {origin} failed due to error:", e)
 
 
 class ExtensionMapper:
     def __init__(self, known_types={}, default="other", api_url="", name_regex=""):
+        """
+        Basic extension mapper, which relays on external website.
+        """
         self.known_types = known_types
         self.default = default
         self.api_url = api_url
@@ -74,16 +91,44 @@ class ExtensionMapper:
 
 
 if __name__ == "__main__":
-    source_folder = destination_folder = sys.argv[1]
-    excluded = (r".*\.crdownload$",)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", help="Source folder")
+    parser.add_argument(
+        "-d", "--destination", help="Destination folder", default="source"
+    )
+    parser.add_argument("-l", "--logfile", help="Log file")
+    parser.add_argument("-v", "--debug", help="Run in debug mode", action="store_true")
+    args = parser.parse_args()
+
+    if args.destination == "source":
+        args.destination = args.source
+
+    if args.logfile:
+        handler = logging.FileHandler(args.logfile)
+    else:
+        handler = logging.StreamHandler(sys.stdout)
+
+    if args.debug:
+        handler.setLevel(logging.DEBUG)
+        log.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+
+    excluded = (r".*\.crdownload$", r".*\.temp$", r".*\.part.*")
     api_url = "https://fileinfo.com/extension/"
     name_regex = r"""<td>Category</td><td><a href="/filetypes/.+?">(.*?)</a></td>"""
-    known_types = {}
+    known_types = {"pdf": "PDF", "skp": "SketchUp"}
+    for ext in ("dwg", "dxf"):
+        known_types[ext] = "AutoCAD"
 
     ext_mapper = ExtensionMapper(known_types, "other", api_url, name_regex)
-    handler = NewFileHander(ext_mapper, destination_folder, excluded)
+    handler = NewFileHander(ext_mapper, args.destination, excluded)
     observer = Observer()
-    observer.schedule(handler, path=source_folder, recursive=False)
+    observer.schedule(handler, path=args.source, recursive=False)
     observer.start()
 
     try:
